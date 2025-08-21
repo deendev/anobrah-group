b57a55042255d738c01b14ee3b542e727972790b
deendev <143696915+deendev@users.noreply.github.com>
2025-08-21 20:51:12 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex min-h-full flex-col bg-white">
+      <body className="flex min-h-full flex-col bg-white font-sans">
         <Navbar />
         <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
       </body>

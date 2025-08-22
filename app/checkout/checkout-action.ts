19e09a22568e7c555561e88a1b03ecfa051cb0f4
deendev <143696915+deendev@users.noreply.github.com>
2025-08-23 00:12:05 +0200

    Fix issues

--- app/checkout/checkout-action.ts
+++ app/checkout/checkout-action.ts
@@ -4,25 +4,32 @@ import { CartItem } from "@/store/cart-store";
 import { redirect } from "next/navigation";
 
 export const checkoutAction = async (formData: FormData): Promise<void> =>{
-    const itemsJson = formData.get("items") as string
-    const items = JSON.parse(itemsJson);
-    const line_items = items.map((item: CartItem) => ({
-        price_data: {
-            currency: "NGN",
-            product_data: {name: item.name},
-            unit_amount: item.price,
+    const itemsJson = formData.get("items") as string;
+    const items = JSON.parse(itemsJson);
+    const line_items = items.map((item: CartItem) => ({
+        price_data: {
+            currency: "NGN",
+            product_data: {name: item.name},
+            unit_amount: item.price,
+        },
+        quantity: item.quantity,
+    }));
+
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items,
+        mode: "payment",
+        success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
+        cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
+        // Add these two lines to collect shipping and phone details
+        shipping_address_collection: {
+            allowed_countries: ['NG'], // Specify the countries you ship to
+        },
+        phone_number_collection: {
+            enabled: true,
         },
-        quantity: item.quantity,
-    }));
+    });
 
-
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items,
-            mode: "payment",
-            success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
-            cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
-    });
-
-    redirect(session.url!)
+    redirect(session.url!);
 };

8d8a2c15329ce8a73489a5f371240a3c382a4819
deendev <143696915+deendev@users.noreply.github.com>
2025-08-22 10:39:17 +0200

    Fix issues

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -14,11 +14,11 @@ export const ProductCard = ({ product }: Props) => {
   return (
     <Link
       href={`/products/${product.id}`}
-      className="block transform transition-transform duration-300 hover:-translate-y-2 max-w-md mx-auto"
+      className="block h-full transform transition-transform duration-300 hover:-translate-y-1 md:hover:-translate-y-2 max-w-sm mx-auto"
     >
       <Card className="group h-full flex flex-col overflow-hidden rounded-xl shadow-lg transition-shadow duration-300 hover:shadow-2xl border border-gray-200">
         {product.images && product.images[0] && (
-          <div className="relative h-70 w-full  overflow-hidden">
+          <div className="relative h-60 w-full overflow-hidden">
             <Image
               alt={product.name}
               src={product.images[0]}
@@ -29,28 +29,28 @@ export const ProductCard = ({ product }: Props) => {
           </div>
         )}
 
-        <div className="flex flex-col p-6 flex-grow">
-          <CardHeader className="p-0 mb-2">
-            <CardTitle className="text-xl font-bold text-gray-800 line-clamp-2">
+        <div className="flex flex-col p-4 flex-grow">
+          <CardHeader className="p-0 mb-1">
+            <CardTitle className="text-lg font-bold text-gray-800 line-clamp-2">
               {product.name}
             </CardTitle>
           </CardHeader>
           <CardContent className="p-0 flex-grow flex flex-col justify-between">
             {product.description && (
-              <p className="text-gray-600 text-sm mb-4 line-clamp-3">
+              <p className="text-xs text-gray-600 mb-2 line-clamp-3">
                 {product.description}
               </p>
             )}
 
             <div className="mt-auto">
               {price && price.unit_amount && (
-                <p className="text-2xl font-extrabold text-gray-900">
+                <p className="text-xl font-extrabold text-gray-900">
                   ₦{(price.unit_amount / 100).toFixed(2)}
                 </p>
               )}
             </div>
 
-            <Button className="mt-4 w-full bg-black text-white hover:bg-gray-800 transform transition-transform duration-300 hover:scale-105">
+            <Button className="mt-2 w-full bg-black text-white hover:bg-gray-800 transform transition-transform duration-300 hover:scale-105">
               View Details
             </Button>
           </CardContent>
